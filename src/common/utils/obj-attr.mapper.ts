982c1d86dccb92f8a86b8a606f8b9f9c144801bc
hahunavth <vuthanhha.2001@gmail.com>
2023-03-06 18:01:05 +0700

    Fix objAttrMapper

--- src/common/utils/obj-attr.mapper.ts
+++ src/common/utils/obj-attr.mapper.ts
@@ -4,8 +4,12 @@
  * @param source
  * @returns
  */
-export function objAttrMapper(target, source) {
-  Object.keys(source).forEach((key, i, a) => {
+export function objAttrMapper(target: object, source: object) {
+  // console.log(Object.getOwnPropertyNames(target));
+  Object.getOwnPropertyNames(source).forEach((key, i, a) => {
+    const attributeType = Reflect.getMetadata('design:type', target, key);
+    // console.log(` >${key}: ${attributeType}`);
+    // console.log(`${typeof source[key]}`);
     if (source[key] !== undefined) {
       target[key] = source[key];
     } else {
